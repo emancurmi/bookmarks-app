49a24bf30c469115dea237690d276d62f4ba653d
Eman Curmi <emancurmi@gmail.com>
2020-07-05 01:31:28 -0400

    bug fixes and connected to bookmarks server app

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export default {
-  //API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
+ // API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
     API_ENDPOINT: 'http://localhost:8000/api/bookmarks',
-    API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  //API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+    API_TOKEN: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+ // API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
 
 }
